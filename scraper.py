686ac6f4ddc9a5f60c684fa98ea74ef41380c22f
jacksongs <jacksongs@gmail.com>
2015-05-31 15:53:36 +1000

    First commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,68 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# This scraper collects all the AFL players from each of the club websites
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import dateutil.parser
+import re
+import pandas as pd
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+afl = requests.get("http://www.afl.com.au")
+
+aflsoup = BeautifulSoup(afl.content)
+
+aflul = aflsoup.find("ul",class_="team-logos")
+afllis = aflul.find_all("li")
+
+teams = {}
+players = {}
+for a in afllis:
+	teams[a.a.text] = a.a.get("href")
+
+for t in teams.keys():
+	teampage = requests.get(teams[t])
+	teamsoup = BeautifulSoup(teampage.content)
+	nav = teamsoup.find("div",class_="nav")
+	navlinks = nav.find_all("a")
+	playerlink = None
+	for n in navlinks:
+		if t == u"North Melbourne":
+			players[t] = teams[t]+"players"
+		elif t == u"Geelong Cats":
+			players[t] = "http://www.geelongcats.com.au/football/players/seniors"
+		else:
+			if "player" in n.get("href"):
+				if "senior" in n.get("href"):
+					players[t] = teams[t]+n.get("href")[1:]
+
+data = []
+
+for p in players.keys():
+	playerpage = requests.get(players[p])
+	playersoup = BeautifulSoup(playerpage.content)
+	tbody = playersoup.find("tbody")
+	thead = playersoup.find("thead")
+	cats = []
+	ths = thead.find_all("th")
+	for th in ths:
+		if th.get("class") != None:
+			cats.append(th.get("class")[0])
+	trs = tbody.find_all("tr")
+	for tr in trs:
+		player = {}
+		tds = tr.find_all("td")
+		for i,td in enumerate(tds):
+			if i == 9:
+				player['glsyear'] = td.text
+			elif i == 10:
+				player['glscareer'] = td.text
+			else:
+				player[cats[i]] = td.text
+		player['club'] = p
+		player['year'] = datetime.datetime.now().year
+		data.append(player)
+
+df = pd.DataFrame(data)
+df.to_csv('players.csv',encoding='utf-8')
+scraperwiki.sqlite.save(unique_keys=["name","club","year"],data=data,table_name='players')
